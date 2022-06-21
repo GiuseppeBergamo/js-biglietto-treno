99c9c5f936068801ea947053152f3865664ff34d
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-06-21 16:03:19 +0200

    added js comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("JS OK")
+
+
+/*
+1- chiedo numero km
+2- chiedo età
+3- calcolare il prezzo del biglietto
+4- se minorenne, sconto del 20%
+5- se over 65, sconto del 40%
+6- output prezzo 
+*/
